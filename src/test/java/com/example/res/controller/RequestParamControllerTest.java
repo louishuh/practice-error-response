7e54b195d84887f7db4368f5820dca6eef8a4574
louis <louis@woowahan.com>
2023-10-25 20:23:31 +0900

    테스트시 디버깅이 용이하게 timeout 시간을 길게 변경

--- src/test/java/com/example/res/controller/RequestParamControllerTest.java
+++ src/test/java/com/example/res/controller/RequestParamControllerTest.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @Slf4j
 @WebFluxTest
+@AutoConfigureWebTestClient(timeout = "86400") // 하루
 class RequestParamControllerTest {
 
     @Autowired
